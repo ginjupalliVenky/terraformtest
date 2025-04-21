49608aff626662eb777a9c3055d747d689c346fe
ginjupalliVenky <ginjupalli93@gmail.com>
2025-04-21 15:23:13 +0530

    disc

--- task03/outputs.tf
+++ task03/outputs.tf
@@ -1,11 +1,14 @@
 output "rg_id" {
   value = azurerm_resource_group.this.id
+  description = "rg name"
 }
 
 output "sa_blob_endpoint" {
   value = azurerm_storage_account.this.primary_blob_endpoint
+  description = "sa enpoint"
 }
 
 output "vnet_id" {
   value = azurerm_virtual_network.this.id
+  description = "vnet id"
 }
