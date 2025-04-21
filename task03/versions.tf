60180e143c86432681e14a126be778d217e1c41c
ginjupalliVenky <ginjupalli93@gmail.com>
2025-04-21 14:54:14 +0530

    arm updated

--- task03/versions.tf
+++ task03/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = ">= 3.110.0"
     }
   }
 
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.5.7"
 }
 
 provider "azurerm" {
